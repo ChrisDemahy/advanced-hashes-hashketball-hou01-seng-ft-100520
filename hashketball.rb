af6568f6c7e7466afed805d238a24ae1b4c35864
Christopher Demahy <demahyc@gmail.com>
2020-10-01 18:32:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -178,8 +178,11 @@ end
 # team - string of the name of the team
 # returns an array of all the numbers on the team
 def player_numbers(team)
-  game_hash[:away][:players].map { |e| e[:number] } if game_hash[:away][:team_name] == team
-  game_hash[:home][:players].map { |e| e[:number] } if game_hash[:home][:team_name] == team 
+  if game_hash[:away][:team_name] == team 
+    game_hash[:away][:players].map { |e| e[:number] } 
+  else
+    game_hash[:home][:players].map { |e| e[:number] }
+  end
 end
 
 # Find how many rebounds the player with the biggest shoe has
