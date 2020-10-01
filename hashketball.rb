93911df85c39abb909a4a875661e9a2579ff3f4b
Christopher Demahy <demahyc@gmail.com>
2020-10-01 18:31:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -178,7 +178,7 @@ end
 # team - string of the name of the team
 # returns an array of all the numbers on the team
 def player_numbers(team)
-  game_hash[:away][:players].map { |e| e[:number] } if game_hash[:away][:team_name] == team 
+  game_hash[:away][:players].map { |e| e[:number] } if game_hash[:away][:team_name] == team
   game_hash[:home][:players].map { |e| e[:number] } if game_hash[:home][:team_name] == team 
 end
 
