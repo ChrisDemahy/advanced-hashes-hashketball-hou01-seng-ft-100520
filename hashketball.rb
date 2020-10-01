66adc3de48e02b1a129dcb515700da7fc9b5021e
Christopher Demahy <demahyc@gmail.com>
2020-10-01 17:08:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -165,5 +165,5 @@ end
 # team - string of the team name
 # returns an array of the colors
 def team_colors(team)
-  game_hash[:away][:team_name] == team ? game_hash[:away][:team_colors] : game_hash[:home][:team_colors]
+  game_hash[:away][:team_name] == team ? return game_hash[:away][:team_colors] : return game_hash[:home][:team_colors]
 end
